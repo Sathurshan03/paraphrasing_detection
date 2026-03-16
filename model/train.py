86895dce1391b6c3ba14ee8eed1bed02d667ce66
Omar <alamo2@mcmaster.ca>
2026-03-15 22:08:29 -0400

    more fixes

--- model/train.py
+++ model/train.py
@@ -24,10 +24,10 @@ def train(data_loader, val_data_loader, model_l, optimizer_l, criterion_l, devic
         model_l.train()
         loss_epoch = 0
         tqdm_loader = tqdm(data_loader)
-        for batch_index, (data, targets) in enumerate(tqdm_loader):
-            sentence1 = data['sentence1'].to(device_l)
-            sentence2 = data['sentence2'].to(device_l)
-            labels = targets['label'].float().to(device_l)
+        for batch_index, batch in enumerate(tqdm_loader):
+            sentence1 = batch['sentence1'].to(device_l)
+            sentence2 = batch['sentence2'].to(device_l)
+            labels = torch.tensor([float(l) for l in batch['label']], device=device_l)
 
             optimizer_l.zero_grad()
 
@@ -61,10 +61,10 @@ def validate(val_loader_l, model_l, criterion_l, device_l):
     model_l.eval()
     val_loss = 0
     with torch.no_grad():
-        for batch_index, (data, targets) in enumerate(tqdm(val_loader_l)):
-            sentence1 = data['sentence1'].to(device_l)
-            sentence2 = data['sentence2'].to(device_l)
-            labels = targets['label'].float().to(device_l)
+        for batch_index, batch in enumerate(tqdm(val_loader_l)):
+            sentence1 = batch['sentence1'].to(device_l)
+            sentence2 = batch['sentence2'].to(device_l)
+            labels = torch.tensor([float(l) for l in batch['label']], device=device_l)
 
             outputs = model_l(sentence1, sentence2).squeeze()
             loss = criterion_l(outputs, labels)
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     dataset = DatasetParaphrase('dataset.csv')
-    train_loader, val_loader, test_loader = dataset.get_data_loaders(batch_size=16, num_workers=6, pin_memory=True)
+    train_loader, val_loader, test_loader = dataset.get_data_loaders(batch_size=1, num_workers=6, pin_memory=True)
 
     model = RNN_FastText()
     model.to(device)
